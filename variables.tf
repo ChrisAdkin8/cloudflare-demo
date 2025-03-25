5b02bedcad888db6124526096b81d8c766e1d086
Chris Adkin <chris@exadat.co.uk>
2025-03-25 22:40:46 +0000

    Some default variable settings taken out of variables.tf file and placed in terraform.tfvars

--- variables.tf
+++ variables.tf
@@ -5,16 +5,13 @@ variable ami {
 
 variable domain {
   type    = string
-  default = "chris73adkin.shop"
 }
 
 # Edit DNS API token
 variable api_token {
   type    = string
-  default = "bptdzArJwpPKHA8jhip17mlCAWnkDTezKJdI6hKN"
 }
 
 variable zone_id {
   type    = string
-  default = "06b4ae52d41c6bbe17fdb284d7922292"
 }
